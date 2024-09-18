17869891cb72c8e1871cf41add4293bc855099ac
NikUrukalo <nik.urukalo@gmail.com>
2024-09-18 22:45:53 +0200

    dodal vzorec za posamezno igro

--- posamezen_blok.py
+++ posamezen_blok.py
@@ -13,3 +13,18 @@ with open('podatki-o-igrah.html', 'r', encoding='utf-8') as f:
 bloki = posamezen_blok.findall(stran)
 
 print(f"Število najdenih blokov: {len(bloki)}")
+
+posamezna_igra = re.compile(
+    r'<a name=.(?P<rang>\d+.)></a>'
+    r'alt=.Board Game: (?P<ime>.*?)"'
+    r'<span class='smallerfont dull'>.(?P<leto>\d+).</span>'
+    r'<a\s+href="/boardgame/(?P<id>\d+)/'
+    r'<p class="smallefont dull" style="margin: 2px 0 0 0;">\n(?P<opis>*.*\n.*?)\n.*?</p>'
+    r'<td class='collection_bggrating' align='center'>\n(?P<ocena_strani>.*)\n\n.*?<td class='collection_bggrating' align='center'>\n.*\n\n.*?<td class='collection_bggrating' align='center'>\n.*\n\n.*?'
+    r'<td class='collection_bggrating' align='center'>\n.*\n\n.*?<td class='collection_bggrating' align='center'>\n(?P<povprecna_ocena>.*)\n\n.*?<td class='collection_bggrating' align='center'>\n.*\n\n.*?'
+    r'<td class='collection_bggrating' align='center'>\n.*\n\n.*?<td class='collection_bggrating' align='center'>\n.*\n\n.*?<td class='collection_bggrating' align='center'>\n(?P<st_glasov>.*)\n\n.*?',
+    flags=re.DOTALL
+)
+
+
+
