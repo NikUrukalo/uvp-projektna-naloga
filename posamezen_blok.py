f77ab434f3420821f9d0d969b9f2a39c234fcec4
NikUrukalo <nik.urukalo@gmail.com>
2024-09-19 17:33:24 +0200

    popravil, da mi ustrezno najde podatke za 1. stran

--- posamezen_blok.py
+++ posamezen_blok.py
@@ -1,9 +1,7 @@
 import re
 
 posamezen_blok = re.compile(
-    r'<td class=.collection_thumbnail.>'
-    r'.*?'
-    r'<td class=.collection_shop.>',
+    r"<tr id='row_'>.*?<td class='collection_shop'>",
     flags=re.DOTALL
 )
 
@@ -15,16 +13,27 @@ bloki = posamezen_blok.findall(stran)
 print(f"Število najdenih blokov: {len(bloki)}")
 
 posamezna_igra = re.compile(
-    r'<a name=.(?P<rang>\d+.)></a>'
-    r'alt=.Board Game: (?P<ime>.*?)"'
-    r'<span class='smallerfont dull'>.(?P<leto>\d+).</span>'
-    r'<a\s+href="/boardgame/(?P<id>\d+)/'
-    r'<p class="smallefont dull" style="margin: 2px 0 0 0;">\n(?P<opis>*.*\n.*?)\n.*?</p>'
-    r'<td class='collection_bggrating' align='center'>\n(?P<ocena_strani>.*)\n\n.*?<td class='collection_bggrating' align='center'>\n.*\n\n.*?<td class='collection_bggrating' align='center'>\n.*\n\n.*?'
-    r'<td class='collection_bggrating' align='center'>\n.*\n\n.*?<td class='collection_bggrating' align='center'>\n(?P<povprecna_ocena>.*)\n\n.*?<td class='collection_bggrating' align='center'>\n.*\n\n.*?'
-    r'<td class='collection_bggrating' align='center'>\n.*\n\n.*?<td class='collection_bggrating' align='center'>\n.*\n\n.*?<td class='collection_bggrating' align='center'>\n(?P<st_glasov>.*)\n\n.*?',
+    r'<a name="(?P<rang>\d+)"></a>.*?'
+    r'alt="Board Game: (?P<ime>.*?)".*?'
+    r'<span class=\'smallerfont dull\'>\((?P<leto>\d{4})\)</span>.*?'
+    r'<p class="smallefont dull".*?>(?P<opis>.*?)</p>.*?'
+    r'<td class=\'collection_bggrating\' align=\'center\'>\s*(?P<ocena_strani>\d+\.\d+)\s*</td>.*?'
+    r'<td class=\'collection_bggrating\' align=\'center\'>\s*(?P<povprecna_ocena>\d+\.\d+)\s*</td>.*?'
+    r'<td class=\'collection_bggrating\' align=\'center\'>\s*(?P<stevilo_glasov>\d+)\s*</td>',
     flags=re.DOTALL
 )
 
+def izloci_podatke_igre(blok):
+    igra = posamezna_igra.search(blok).groupdict()
+    igra['rang'] = int(igra['rang'])
+    igra['ime'] = igra['ime'].strip()
+    igra['leto'] = int(igra['leto'])
+    igra['opis'] = re.sub(r'\s+', ' ', igra['opis']).strip()
+    igra['ocena_strani'] = float(igra['ocena_strani'])
+    igra['povprecna_ocena'] = float(igra['povprecna_ocena'])
+    igra['stevilo_glasov'] = int(igra['stevilo_glasov'])
+    return igra
 
-
+for blok in bloki:
+    podatki_igre = izloci_podatke_igre(blok)
+    print(podatki_igre)
